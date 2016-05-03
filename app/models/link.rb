b2bdf5373d25848e172698f0286e4452dbcb4cac
paulwallis42 <paulwallis42@gmail.com>
2016-05-03 16:00:54 +0100

    test & dev environment established

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_16_test")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_16_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
