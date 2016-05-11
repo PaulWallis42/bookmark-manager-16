3c07c245a6dc3ef2a78d84896f5d672c1cb3e3e0
paulwallis42 <paulwallis42@gmail.com>
2016-05-11 09:41:11 +0100

    Errors created for duplicate e mails

--- spec/feature/user_management_spec.rb
+++ spec/feature/user_management_spec.rb
@@ -14,15 +14,25 @@ feature 'User sign up' do
   scenario 'failing confirmation password does not redirect page' do
     sign_up(password_confirmation: 'wrong')
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Password and confirmation password do not match')
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   scenario 'I can not sign up without an e mail address' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'I can\'t sign up with an invalid e mail address' do
     expect {sign_up(email: 'invalid@email')}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 
 end
